3f8b6027b85e0125b8951d7615e2ae9e8b5b814d
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-18 19:17:23 +0200

    Add files via upload

---
+++ JavaScriptFundamentals/01-IntroToJavaScript/02-SumAndVAT.js
@@ -0,0 +1,10 @@
+function calcSumVAT(input){
+    let sum = 0
+    let countOfNumbers = input.length;
+    for (let i = 0; i < countOfNumbers; i++){
+        sum += Number(input[i]);
+    }
+    console.log('sum = ' + sum);
+    console.log('VAT = ' + sum * 0.2);
+    console.log('total = ' + (sum * 1.2))
+}
