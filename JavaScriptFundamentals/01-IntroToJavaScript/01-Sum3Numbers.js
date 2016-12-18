9bf79c96346292169ac8e9d01723a88ce98eab8f
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-18 16:54:18 +0200

    Add files via upload

---
+++ JavaScriptFundamentals/01-IntroToJavaScript/01-Sum3Numbers.js
@@ -0,0 +1,9 @@
+function sumNumbers (input) {
+    let num1 = Number(input[0]);
+    let num2 = Number(input[1]);
+    let num3 = Number(input[2]);
+    let sum = num1 + num2 + num3;
+    return sum;
+}
+
+//sumNumbers (['5', '10', '15'])
