6ec049e6a402c5b237fdb9ea5521b28f67759785
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-19 12:25:40 +0200

    Update 07-NextDay.js

--- JavaScriptFundamentals/01-IntroToJavaScript/07-NextDay.js
+++ JavaScriptFundamentals/01-IntroToJavaScript/07-NextDay.js
@@ -3,3 +3,5 @@
     let nextDay = new Date(someDate.getTime() + 24 * 60 * 60 * 1000)
      return nextDay.getFullYear() + '-' + (nextDay.getMonth() + 1) + '-' + nextDay.getDate()
   }
+
+// nextDay(['2016', '9', '30'])
