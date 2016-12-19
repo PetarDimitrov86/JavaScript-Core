bda81806f8bafe032d2ea1f346970e1d719f5bd9
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-19 12:25:06 +0200

    Add files via upload

---
+++ JavaScriptFundamentals/01-IntroToJavaScript/07-NextDay.js
@@ -0,0 +1,5 @@
+ function nextDay([year, month, date]){
+    let someDate = new Date(year, month - 1, date);
+    let nextDay = new Date(someDate.getTime() + 24 * 60 * 60 * 1000)
+     return nextDay.getFullYear() + '-' + (nextDay.getMonth() + 1) + '-' + nextDay.getDate()
+  }
