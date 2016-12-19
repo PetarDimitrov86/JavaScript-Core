31c0b0824505890f0def7edcb2dd377e8f860400
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-19 22:38:39 +0200

    Add files via upload

---
+++ JavaScriptFundamentals/02-ControlFlowLogic/09-ColourfulNumbers.js
@@ -0,0 +1,17 @@
+function printNumbers([numberOfLines]) {
+    let result = '<ul>\r\n';
+    for (let i = 1; i <= numberOfLines; i++){
+        result += '  <li><span style=\'color:';
+        if (i % 2 == 0){
+            result += 'blue';
+        }
+        else {
+            result += 'green';
+        }
+        result += `'\'>${i}</span></li>\r\n`;
+    }
+    result+='</ul>'
+    return result;
+}
+
+// console.log(printNumbers([10]));
