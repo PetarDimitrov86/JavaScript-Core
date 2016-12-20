ff5c5b19510f0ff67b1ac8ca0a338a43182e9403
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-20 18:59:46 +0200

    Add files via upload

---
+++ JavaScriptFundamentals/01-IntroToJavaScript/Exercises/11-BinaryToDecimal.js
@@ -0,0 +1,13 @@
+function convertToDecimal(binaryNum) {
+    let arrAsString = binaryNum.toString();
+    let result = 0;
+    let power = 7;
+    for (let i = 0; i < arrAsString.length; i++) {
+        result += arrAsString.charAt(i) * Math.pow(2, power);
+        power--;
+    }
+    return result;
+}
+
+// console.log(convertToDecimal(['00001001']));
+// console.log(convertToDecimal(['11110000']));
