cb84393112e817f1a2949787953730fcecae2b63
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-20 23:28:42 +0200

    Update 05-GradsToDegrees.js

--- JavaScriptFundamentals/01-IntroToJavaScript/Exercises/05-GradsToDegrees.js
+++ JavaScriptFundamentals/01-IntroToJavaScript/Exercises/05-GradsToDegrees.js
@@ -1,7 +1,7 @@
 function calcDegrees(grads){
     let degrees = grads * 0.9;
     if (Number(grads) < 0)
-        degrees = 360 - Math.abs(degrees % 360);
+        degrees = 360 - Math.abs(degrees % 360); // degrees += 360;
     return degrees % 360;
 }
 
