c7c77bbdd87e86b01d567ba04dca7aa5e13312a0
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-26 12:34:25 +0200

    Update 08-BiggestElementInMatrix.js

--- JavaScriptFundamentals/04-ArraysAndMatrices/Lab/08-BiggestElementInMatrix.js
+++ JavaScriptFundamentals/04-ArraysAndMatrices/Lab/08-BiggestElementInMatrix.js
@@ -22,3 +22,15 @@ function biggestElement(matrixRows){
 //              biggestNum = matrix[row][col];
 //      }
 //  }
+
+// Version 3 : Author Solution
+// function biggestElement(matrixRows) {
+//     let matrix = matrixRows.map(row => row.split(' ').map(Number));
+//     let biggestNum = Number.NEGATIVE_INFINITY;
+// 
+//     matrix.forEach(
+//         r => r.forEach(
+//             c => biggestNum = Math.max(biggestNum, c)));
+// 
+//     return biggestNum;
+// }
