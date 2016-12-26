a66dc943fe7ae0837e7c6bf052630be76467c147
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-26 16:43:03 +0200

    Update 05-ExtractNon-decreasingSubsequence.js

--- JavaScriptFundamentals/04-ArraysAndMatrices/Exercises/05-ExtractNon-decreasingSubsequence.js
+++ JavaScriptFundamentals/04-ArraysAndMatrices/Exercises/05-ExtractNon-decreasingSubsequence.js
@@ -12,4 +12,21 @@ function extractSeq(input){
     return result.join('\n');
 }
 
-console.log(extractSeq([1, 3, 8, 4, 10, 12, 3, 2, 24]));
+// console.log(extractSeq([1, 3, 8, 4, 10, 12, 3, 2, 24]));
+
+// Version two, creating custom filter Func
+// function extractSeq(input){
+//     let arr = input.map(Number);
+//     let currentMaxEl = Number.NEGATIVE_INFINITY;
+// 
+//     let filteredNums = arr.filter(a => elementCheck(a));
+//     return filteredNums.join('\n');
+// 
+//     function elementCheck(num){
+//         if (currentMaxEl <= num){
+//             currentMaxEl = num;
+//             return true;
+//         }
+//         return false;
+//     }
+// }
