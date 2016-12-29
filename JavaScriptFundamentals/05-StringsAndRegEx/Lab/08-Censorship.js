f43b179225c48860b379d20addb1cef49f75124e
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-29 20:22:51 +0200

    Update 08-Censorship.js

--- JavaScriptFundamentals/05-StringsAndRegEx/Lab/08-Censorship.js
+++ JavaScriptFundamentals/05-StringsAndRegEx/Lab/08-Censorship.js
@@ -9,4 +9,14 @@ function censor(input){
     return text;
 }
 
+// Shorter version 2 using new RegExp()
+// function censor(input){
+//     let text = input[0];
+//     for (let i = 1; i < input.length; i++){
+//         let badWord = input[i];
+//         text = text.replace(new RegExp(badWord, 'g'), '-'.repeat(badWord.length))
+//     }
+//     return text;
+// }
+
 // console.log(censor(['roses are red, violets are blue', ', violets are', 'red']));
