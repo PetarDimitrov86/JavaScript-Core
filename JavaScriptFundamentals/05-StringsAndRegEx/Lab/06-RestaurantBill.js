6020256b7f543d26b124fae6ff234bb0afb6ea1c
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-29 17:58:46 +0200

    Update 06-RestaurantBill.js

--- JavaScriptFundamentals/05-StringsAndRegEx/Lab/06-RestaurantBill.js
+++ JavaScriptFundamentals/05-StringsAndRegEx/Lab/06-RestaurantBill.js
@@ -11,4 +11,12 @@ function printBill(input){
     return `You purchased ${products.join(', ')} for a total sum of ${totalSum}`
 }
 
-console.log(printBill(['Beer Zagorka', '2.65', 'Tripe soup', '7.80','Lasagna', '5.69']));
+// function printBill(input) {
+//     let items = input.filter(x => !Number(x));
+//     let sum = input.filter(x => Number(x))
+//         .map(Number)
+//             .reduce((a,b) => a + b);
+//     console.log(`You purchased ${items.join(', ')} for a total sum of ${sum}`);
+// }
+
+// console.log(printBill(['Beer Zagorka', '2.65', 'Tripe soup', '7.80','Lasagna', '5.69']));
