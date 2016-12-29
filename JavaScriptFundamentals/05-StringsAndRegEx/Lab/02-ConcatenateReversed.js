66383567faf608ced3b2043d0f34a8b65e560bf7
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-29 11:02:35 +0200

    Update 02-ConcatenateReversed.js

--- JavaScriptFundamentals/05-StringsAndRegEx/Lab/02-ConcatenateReversed.js
+++ JavaScriptFundamentals/05-StringsAndRegEx/Lab/02-ConcatenateReversed.js
@@ -8,4 +8,14 @@ function concatReverse(input){
     console.log(concatResult)
 }
 
-concatReverse(['I', 'am', 'student'])
+// concatReverse(['I', 'am', 'student'])
+
+// Version two using JOIN REVERSE ARRAY.FROM
+//function concatenateAndReverse(arr) {
+//let allStrings = arr.join('');
+//let chars = Array.from(allStrings);
+//let revChars = chars.reverse();
+//let revStr = revChars.join('');
+//return revStr;
+//}
+
