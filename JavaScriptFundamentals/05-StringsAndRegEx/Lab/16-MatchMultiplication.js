80d857e426374832bca258779c587c94fe244414
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-30 23:01:59 +0200

    Update 16-MatchMultiplication.js

--- JavaScriptFundamentals/05-StringsAndRegEx/Lab/16-MatchMultiplication.js
+++ JavaScriptFundamentals/05-StringsAndRegEx/Lab/16-MatchMultiplication.js
@@ -8,3 +8,8 @@ function multiply([text]){
 
 console.log(multiply(['My bill: 2*2.50 (beer); 2* 1.20 (kepab); -2  * 0.5 (deposit).' +
 '']));
+
+// Version 2 : author solution
+// function performMultiplications([text]) {
+//     return text.replace(/(-?\d+)\s*\*\s*(-?\d+(\.\d+)?)/g, (match, num1, num2) => Number(num1) * Number(num2));
+// }
