38b2ed15391cc959525f5140cff5765ef0ccda05
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-30 20:34:23 +0200

    Add files via upload

---
+++ JavaScriptFundamentals/05-StringsAndRegEx/Lab/15-FormFiller.js
@@ -0,0 +1,22 @@
+function fillForm(input){
+    let result = '';
+    let username = input.shift();
+    let email = input.shift();
+    let phone = input.shift();
+    let pattern = /(<!\w+!>)|(<@\w+@>)|(<\+\w+\+>)/g;
+    for (let line of input){
+        console.log(line.replace(/<![a-zA-Z]+!>/g, username)
+            .replace(/<@[a-zA-Z]+@>/g, email)
+            .replace(/<\+[a-zA-Z]+\+>/g, phone));
+    }
+}
+
+(fillForm(['Pesho',
+        'pesho@softuni.bg',
+        '90-60-90',
+        'Hello, <!username!>!',
+        'Welcome to your Personal profile.',
+        'Here you can modify your profile freely.',
+        'Your current username is: <!fdsfs!>. Would you like to change that? (Y/N)',
+        'Your current email is: <@DasEmail@>. Would you like to change that? (Y/N)',
+        'Your current phone number is: <+number+>. Would you like to change that? (Y/N)',]));
