beb9da8fefa1af0e99202614b7cffc261d83c262
Petar Dimitrov <lbmikesmith@gmail.com>
2016-12-30 20:36:42 +0200

    Add files via upload

--- JavaScriptFundamentals/05-StringsAndRegEx/Lab/15-FormFiller.js
+++ JavaScriptFundamentals/05-StringsAndRegEx/Lab/15-FormFiller.js
@@ -1,9 +1,5 @@
 function fillForm(input){
-    let result = '';
-    let username = input.shift();
-    let email = input.shift();
-    let phone = input.shift();
-    let pattern = /(<!\w+!>)|(<@\w+@>)|(<\+\w+\+>)/g;
+    let [username, email, phone] = [input.shift(), input.shift(), input.shift()];
     for (let line of input){
         console.log(line.replace(/<![a-zA-Z]+!>/g, username)
             .replace(/<@[a-zA-Z]+@>/g, email)
@@ -11,12 +7,12 @@ function fillForm(input){
     }
 }
 
-(fillForm(['Pesho',
-        'pesho@softuni.bg',
-        '90-60-90',
-        'Hello, <!username!>!',
-        'Welcome to your Personal profile.',
-        'Here you can modify your profile freely.',
-        'Your current username is: <!fdsfs!>. Would you like to change that? (Y/N)',
-        'Your current email is: <@DasEmail@>. Would you like to change that? (Y/N)',
-        'Your current phone number is: <+number+>. Would you like to change that? (Y/N)',]));
+// (fillForm(['Pesho',
+//         'pesho@softuni.bg',
+//         '90-60-90',
+//         'Hello, <!username!>!',
+//         'Welcome to your Personal profile.',
+//         'Here you can modify your profile freely.',
+//         'Your current username is: <!fdsfs!>. Would you like to change that? (Y/N)',
+//         'Your current email is: <@DasEmail@>. Would you like to change that? (Y/N)',
+//         'Your current phone number is: <+number+>. Would you like to change that? (Y/N)',]));
