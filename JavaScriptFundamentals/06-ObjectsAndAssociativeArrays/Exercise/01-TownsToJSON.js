d4ad41ab31bd10deb9f02a59d89975823a114d5e
Petar Dimitrov <lbmikesmith@gmail.com>
2017-01-04 20:59:22 +0200

    Update 01-TownsToJSON.js

--- JavaScriptFundamentals/06-ObjectsAndAssociativeArrays/Exercise/01-TownsToJSON.js
+++ JavaScriptFundamentals/06-ObjectsAndAssociativeArrays/Exercise/01-TownsToJSON.js
@@ -15,15 +15,15 @@ function createJSON(input){
 // console.log(createJSON(['| Town | Latitude | Longitude |', '| Sofia | 42.696552 | 23.32601 |', '| Beijing | 39.913818 | 116.363625 |']))
 
 // Version 2 : Author Solution
-function parseTownsToJSON(towns) {
-    let townsArr = [];
-    for (let town of towns.slice(1)) {
-        let [empty, townName, lat, lng] = town.split(/\s*\|\s*/);
-        let townObj = { Town: townName, 
-                       Latitude: Number(lat), 
-                       Longitude: Number(lng) 
-                      };
-        townsArr.push(townObj);
-        }
-    return JSON.stringify(townsArr);
-}
+// function parseTownsToJSON(towns) {
+//     let townsArr = [];
+//     for (let town of towns.slice(1)) {
+//         let [empty, townName, lat, lng] = town.split(/\s*\|\s*/);
+//         let townObj = { Town: townName, 
+//                        Latitude: Number(lat), 
+//                        Longitude: Number(lng) 
+//                       };
+//         townsArr.push(townObj);
+//         }
+//     return JSON.stringify(townsArr);
+// }
