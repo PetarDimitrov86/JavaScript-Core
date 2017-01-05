ad41b94973790e80a591ce4a54710f7b23be091b
Petar Dimitrov <lbmikesmith@gmail.com>
2017-01-05 15:00:22 +0200

    Update 05-CountWordsInAText.js

--- JavaScriptFundamentals/06-ObjectsAndAssociativeArrays/Lab/05-CountWordsInAText.js
+++ JavaScriptFundamentals/06-ObjectsAndAssociativeArrays/Lab/05-CountWordsInAText.js
@@ -1,6 +1,7 @@
 function countWords([data]){
     let words = {};
     let dataArr = data.split(/\W+/).filter(w => w != '');
+                //data.split(/[^A-Za-z0-9_]+/).filter(w => w != '');
     for (let word of dataArr){
         if (words[word] == undefined)
             words[word] = 0;
