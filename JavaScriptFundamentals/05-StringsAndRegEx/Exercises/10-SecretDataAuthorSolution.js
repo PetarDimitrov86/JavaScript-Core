80ae94e9486507a71c688758d7ff66a4c698b7a3
Petar Dimitrov <lbmikesmith@gmail.com>
2017-01-09 13:07:59 +0200

    Update 10-SecretDataAuthorSolution.js

--- JavaScriptFundamentals/05-StringsAndRegEx/Exercises/10-SecretDataAuthorSolution.js
+++ JavaScriptFundamentals/05-StringsAndRegEx/Exercises/10-SecretDataAuthorSolution.js
@@ -1,10 +1,10 @@
 function hideInfo(input){
     let finalText = input.join('\n');
 
-    let pattern1 = /(\*[A-Z][A-Za-z]*)(?=\s|$|\n)/g;
-    let pattern2 = /(\+[0-9-]{10})(?=\s|$|\n)/g;
-    let pattern3 = /(![A-Za-z0-9]+)(?=\s|$|\n)/g;
-    let pattern4 = /(_[A-Za-z0-9]+)(?=\s|$|\n)/g;
+    let pattern1 = /(\*[A-Z][A-Za-z]*)(?=\s|$)/g;
+    let pattern2 = /(\+[0-9-]{10})(?=\s|$)/g;
+    let pattern3 = /(![A-Za-z0-9]+)(?=\s|$)/g;
+    let pattern4 = /(_[A-Za-z0-9]+)(?=\s|$)/g;
 
     finalText = finalText.replace(pattern1, encrypt);
     finalText = finalText.replace(pattern2, encrypt);
