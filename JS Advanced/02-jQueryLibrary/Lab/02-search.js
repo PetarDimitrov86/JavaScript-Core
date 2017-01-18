57a5f25209bd23b225919819b7d4fd6e8e2bbd64
Petar Dimitrov <lbmikesmith@gmail.com>
2017-01-18 19:53:25 +0200

    Update 02-search.js

--- JS Advanced/02-jQueryLibrary/Lab/02-search.js
+++ JS Advanced/02-jQueryLibrary/Lab/02-search.js
@@ -6,3 +6,16 @@ function search(){
     $(':not(:contains(' + searchPattern + '))').css('font-weight', 'normal');
     $('#result').append(foundItems.length + " matches found.")
 }
+
+// Version 2 : Author Solution
+// function search() {
+//   let searchText = $('#searchText').val();
+//   let matches = 0;
+//   $("#towns li").each((index, item) => {
+//     if (item.textContent.includes(searchText)) {
+//       $(item).css("font-weight", "bold");
+//       matches++;
+//     } else
+//       $(item).css("font-weight", "");
+//   });
+//   $('#result').text(matches + " matches found.");  }
