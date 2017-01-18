ba16d4c98a0c2b4bfde69a35fabfa2938dcc8124
Petar Dimitrov <lbmikesmith@gmail.com>
2017-01-18 14:47:30 +0200

    Add files via upload

---
+++ JS Advanced/01-DOM,BOM,Events/Lab/09-gradient.js
@@ -0,0 +1,12 @@
+function attachGradientEvents() {
+    let box = document.getElementById('gradient');
+    let div = document.getElementById('result');
+    box.addEventListener('mousemove', calcPercent);
+    box.addEventListener('mouseout', function(){ div.textContent = ''; });
+
+    function calcPercent(event){
+        let newMouseX =  event.offsetX;
+        let percent = Math.trunc(newMouseX / 300 * 100);
+        div.textContent = percent + '%';
+    }
+}
