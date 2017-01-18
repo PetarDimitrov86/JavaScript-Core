cd840d2a2ad872c1a15a6b1abcae171279b401c6
Petar Dimitrov <lbmikesmith@gmail.com>
2017-01-18 19:57:39 +0200

    Update 02-search.js

--- JS Advanced/02-jQueryLibrary/Lab/02-search.js
+++ JS Advanced/02-jQueryLibrary/Lab/02-search.js
@@ -1,6 +1,7 @@
 function search(){
     $('#result').text('');
-    let searchPattern = $('#searchText')[0].value;
+    let searchPattern = $('#searchText').val();
+    //let searchPattern = $('#searchText')[0].value; 
     let foundItems =  $('li:contains("' + searchPattern + '")');
     foundItems.css('font-weight', 'bold');
     $(':not(:contains(' + searchPattern + '))').css('font-weight', 'normal');
