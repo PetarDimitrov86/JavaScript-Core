6bf98c7b1ed8e72fed18248901a5e380259b2aae
Petar Dimitrov <lbmikesmith@gmail.com>
2017-01-20 23:09:11 +0200

    Add files via upload

---
+++ JS Advanced/02-jQueryLibrary/Exercise/08-wiki-parser.js
@@ -0,0 +1,11 @@
+function wikiParser(selector){
+    let fixedText = $(selector).text()
+        .replace(/'''(.+?)'''/g, `<b>$1</b>`)
+        .replace(/''(.+?)''/g, `<i>$1</i>`)
+        .replace(/===(.+?)===/g, `<h3>$1</h3>`)
+        .replace(/==(.+?)==/g, `<h2>$1</h2>`)
+        .replace(/=(.+?)=/g, `<h1>$1</h1>`)
+        .replace(/\[\[([^\]\[]+?)\|([^\[\]]+?)]]/g, `<a href="/wiki/$1">$2</a>`)
+        .replace(/\[\[([^\]\[]+?)]]/g, `<a href="/wiki/$1">$1</a>`);
+    $(selector).html(fixedText);
+}
