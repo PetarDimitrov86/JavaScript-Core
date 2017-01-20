cef98917f6a0fafd48bc8f7237da79ee0591c675
Petar Dimitrov <lbmikesmith@gmail.com>
2017-01-20 15:22:18 +0200

    Update 04-book-generator.js

--- JS Advanced/02-jQueryLibrary/Exercise/04-book-generator.js
+++ JS Advanced/02-jQueryLibrary/Exercise/04-book-generator.js
@@ -36,3 +36,33 @@ let createBook = (function generateBooks(){
         }
     }
 }());
+
+//function createBook(selector, title, author, ISBN){
+//    let id = $('div[id*="book"]').toArray().length + 1;     // get the total ammount of divs with id attribute, which contains the string "book", CSS Selector *=
+
+//    let titlePar = $('<p>').addClass('title').text(title);
+//    let authorPar = $('<p>').addClass('author').text(author);
+//    let isbnPar = $('<p>').addClass('isbn').text(ISBN);
+
+//    let selectButton = $('<button>').css('padding', '5px').css('margin', '2px').text('Select');
+//    let deselectButton = $('<button>').css('padding', '5px').css('margin', '2px').text('Deselect');
+//    selectButton.on('click', selectBook);
+//    deselectButton.on('click', deselectBook);
+
+//    let divContainer = $('<div>')               // append all the paragraphs and buttons, and finally append the div to the given id wrapper
+//        .append(titlePar)
+//        .append(authorPar)
+//        .append(isbnPar)
+//        .append(selectButton)
+//        .append(deselectButton)
+//        .attr('id', 'book' + id)
+//        .appendTo($(selector));
+
+//    function selectBook(){
+//        divContainer.css('border', '2px solid blue');
+//    }
+
+//    function deselectBook(){
+//        divContainer.css('border', 'none');
+//    }
+//}
