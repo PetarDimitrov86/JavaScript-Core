bb4fa9c29d8566425c4f96ba1144cab47cc91968
Petar Dimitrov <lbmikesmith@gmail.com>
2017-01-26 18:50:00 +0200

    Add files via upload

---
+++ JS Advanced/03-FirstClassFunctions/Lab/01-Aggregates - Shorter.js
@@ -0,0 +1,17 @@
+function reducer(numbers){
+    numbers = numbers.map(Number);
+
+    function customReduce(arr, func){
+        let result = arr[0];
+        for (let nextElement of arr.slice(1))
+            result = func(result, nextElement);
+        return result;
+    }
+    console.log(`Sum = ${customReduce(numbers, (a,b) => a + b)}`);
+    console.log(`Min = ${customReduce(numbers, (a,b) => Math.min(a, b))}`);
+    console.log(`Min = ${customReduce(numbers, (a,b) => Math.max(a, b))}`);
+    console.log(`Product = ${customReduce(numbers, (a,b) => a * b)}`);
+    console.log(`Join = ${customReduce(numbers, (a,b) => a.toString() + b.toString())}`);
+}
+
+reducer([2,3,10,5]);
