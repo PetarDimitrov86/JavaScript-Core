5988c59079b91c658765596a5dc59cfbea46cfa3
Petar Dimitrov <lbmikesmith@gmail.com>
2017-01-29 23:07:38 +0200

    Add files via upload

---
+++ JS Advanced/03-FirstClassFunctions/Exercise/02-ArgumentInfo.js
@@ -0,0 +1,16 @@
+function getTypes(){
+    let occurences = new Map;
+    for (let arg of arguments){
+        let type = typeof arg;
+        console.log(type + ": " + arg)
+        if (!occurences.has(type))
+            occurences.set(type, 0);
+        occurences.set(type, occurences.get(type) + 1);
+    }
+    let sortedResult = Array.from(occurences).sort((a,b) => b[1] - a[1]);
+    for (let [type, count] of sortedResult){
+        console.log(type + ' = ' + count)
+    }
+}
+
+getTypes('cat', 42, 534,'dsasa', function () { console.log('Hello world!'); });
