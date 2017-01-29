30c915a59ea1dfc69cf0eb8dd3894bf3e536db99
Petar Dimitrov <lbmikesmith@gmail.com>
2017-01-29 19:34:57 +0200

    Update 02-CommandProcessor.js

--- JS Advanced/03-FirstClassFunctions/Lab/02-CommandProcessor.js
+++ JS Advanced/03-FirstClassFunctions/Lab/02-CommandProcessor.js
@@ -4,10 +4,8 @@ function processCommands(commands) {
         return {
             append: (newText) => text += newText,
             removeStart: (count) => text = text.slice(count),
-            removeEnd: (count) => text =
-                text.slice(0, text.length - count),
-            print: () => console.log(text)
-        }
+            removeEnd: (count) => text = text.slice(0, text.length - count),
+            print: () => console.log(text) }
     })();
     for (let cmd of commands) {
         let [cmdName, arg] = cmd.split(' ');
