2fd720507ecf2c3ed340cb2da5b9e4ce1e85691b
Petar Dimitrov <lbmikesmith@gmail.com>
2017-02-03 23:20:07 +0200

    Update 02-Fibonacci.js

--- JS Advanced/04-ObjectComposition/Lab/02-Fibonacci.js
+++ JS Advanced/04-ObjectComposition/Lab/02-Fibonacci.js
@@ -14,6 +14,26 @@ let fibNums = (function (){
 
 console.log(fibNums(5));
 
+// Modified version to properly use IIFE
+
+// function getFibNums(num) {
+//     let fibNum = (function () {
+//         let f1 = 0;
+//         let f2 = 1;
+//         return () => {
+//             let numToReturn = f1 + f2;
+//             f1 = f2;
+//             f2 = numToReturn;
+//             return numToReturn
+//         }
+//     })();
+//     let fibNumbers = [1];
+//     for (let i = 0; i < num - 1; i++) {
+//         fibNumbers.push(fibNum())
+//     }
+//     return fibNumbers
+// }
+
 // Author Solution:
 // let fib = (() => {
 //   let f0 = 0, f1 = 1;
