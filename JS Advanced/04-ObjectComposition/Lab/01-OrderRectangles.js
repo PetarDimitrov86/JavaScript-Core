effe98076a6969255f371014cbce0818c4169ba5
Petar Dimitrov <lbmikesmith@gmail.com>
2017-02-03 15:37:35 +0200

    Add files via upload

---
+++ JS Advanced/04-ObjectComposition/Lab/01-OrderRectangles.js
@@ -0,0 +1,24 @@
+function orderRectangles(arr){
+    let allRects = [];
+    for (let rectangle of arr){
+        let rect = {
+            width: rectangle[0],
+            height: rectangle[1],
+            area:  () => rect.width * rect.height,
+            compareTo : function (other){
+                if (this.area() > other.area())              // DESCENDING SORT
+                    return -1;
+                if (this.area() < other.area())
+                    return 1;
+                if (this.width > other.width)                // DESCENDING SORT
+                    return -1;
+                if (this.width < other.width)
+                    return 1;
+                return 0;
+            }
+        };
+        allRects.push(rect);
+    }
+    return allRects.sort((r1, r2) => r1.compareTo(r2));
+}
+console.log(orderRectangles([[10,5],[5,12]]));
