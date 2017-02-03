47b9659a2bde1e8b6f4e348d0add7eea451878c6
Petar Dimitrov <lbmikesmith@gmail.com>
2017-02-03 17:18:44 +0200

    Add files via upload

---
+++ JS Advanced/04-ObjectComposition/Lab/02-Fibonacci.js
@@ -0,0 +1,16 @@
+let fibNums = (function (){
+    let fibNums = [1];
+    let f1 = 0;
+    let f2 = 1;
+    return function(num){
+        for (let i = 0; i < num - 1; i++){
+            fibNums.push(f1 + f2);
+            f1 = f2;
+            f2 = fibNums[fibNums.length - 1]
+        }
+        return fibNums
+    }
+})();
+
+
+console.log(fibNums(5));
