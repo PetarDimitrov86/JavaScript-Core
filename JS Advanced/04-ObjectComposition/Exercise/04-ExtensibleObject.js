6a84efffee77a90bceb2b763997848c0d2a4cc18
Petar Dimitrov <lbmikesmith@gmail.com>
2017-02-04 23:07:38 +0200

    Add files via upload

---
+++ JS Advanced/04-ObjectComposition/Exercise/04-ExtensibleObject.js
@@ -0,0 +1,20 @@
+function solve(){
+    let obj = {};
+    obj.extend = function(template){
+        for (let prop in template){
+            if (typeof template[prop] == 'function')
+                Object.getPrototypeOf(obj)[prop] = template[prop]
+            else
+                obj[prop] = template[prop];
+        }
+
+    };
+    return obj;
+}
+let template = {
+        extensionMethod: function () { console.log('Extension Method')},
+    extensionProperty: 'someString'
+};
+
+let myObj = solve();
+myObj.extend(template);
