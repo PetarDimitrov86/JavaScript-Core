5e82c7a0f93024ff7b92f35056ace87aa24facb9
Petar Dimitrov <lbmikesmith@gmail.com>
2017-02-10 15:34:45 +0200

    Update dom-tests-setup.js

--- JS Advanced/05-UnitTestingWithMocha/dom-tests-setup.js
+++ JS Advanced/05-UnitTestingWithMocha/dom-tests-setup.js
@@ -2,6 +2,7 @@ let expect = require('chai').expect;
 let jsdom = require('jsdom-global')();
 let $ = require('jquery');
 
+// Manually pass the desired html to be tested
 document.body.innerHTML = `<div id="wrapper">
         <input type="text" id="name">
         <input type="text" id="income">
