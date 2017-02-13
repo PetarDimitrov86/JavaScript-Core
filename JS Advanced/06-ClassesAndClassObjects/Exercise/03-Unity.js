3aa9ce4a24d42d05296664447be6e665aa09ea4b
Petar Dimitrov <lbmikesmith@gmail.com>
2017-02-13 16:40:17 +0200

    Add files via upload

---
+++ JS Advanced/06-ClassesAndClassObjects/Exercise/03-Unity.js
@@ -0,0 +1,34 @@
+class Rat{
+    constructor(name) {
+        this.name = name;
+        this.rats = [];
+    }
+
+    unite(otherRat){
+        if (otherRat instanceof Rat)
+            this.rats.push(otherRat);
+    }
+
+    getRats() { return this.rats; }
+
+    toString() {
+        console.log(this.name);
+        console.log(this.rats.map(r => `##${r.name}`).join('\n'))
+    }
+}
+
+let test = new Rat("Pesho");
+console.log(test.toString()); //Pesho
+
+console.log(test.getRats()); //[]
+
+test.unite(new Rat("Gosho"));
+test.unite(new Rat("Sasho"));
+console.log(test.getRats());
+//[ Rat { name: 'Gosho', unitedRats: [] },
+//  Rat { name: 'Sasho', unitedRats: [] } ]
+
+console.log(test.toString());
+// Pesho
+// ##Gosho
+// ##Sasho
