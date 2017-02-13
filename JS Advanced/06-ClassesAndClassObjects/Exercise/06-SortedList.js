061574ff45e302da614f1f9fad18a02e29c1569d
Petar Dimitrov <lbmikesmith@gmail.com>
2017-02-13 17:23:19 +0200

    Add files via upload

---
+++ JS Advanced/06-ClassesAndClassObjects/Exercise/06-SortedList.js
@@ -0,0 +1,22 @@
+class SortedList{
+    constructor() {
+        this.nums = [];
+        this.size = 0;
+    }
+    get(index) {
+    if (index < this.nums.length && index >= 0)
+        return this.nums[index]
+    }
+    add(element) {
+        this.nums.push(element);
+        this.size++;
+        this.nums = this.nums.sort((a, b) => a - b)
+    }
+    remove(index) {
+        if (index < this.nums.length && index >= 0){
+            this.nums.splice(index, 1);
+            this.nums = this.nums.sort((a, b) => a - b);
+            this.size--;
+        }
+    }
+}
