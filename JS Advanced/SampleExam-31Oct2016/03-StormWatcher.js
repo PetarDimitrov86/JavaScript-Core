cecfb8284035bd97bfe5e4c8cff56fe9b20bddce
Petar Dimitrov <lbmikesmith@gmail.com>
2017-02-17 14:04:37 +0200

    Add files via upload

--- JS Advanced/SampleExam-31Oct2016/03-StormWatcher.js
+++ JS Advanced/SampleExam-31Oct2016/03-StormWatcher.js
@@ -1,3 +1,35 @@
-/**
- * Created by Petar on 17.2.2017 г..
- */
+class Record{
+    constructor(temperature, humidity, pressure, windSpeed){
+        this.id = Record.counter();
+        this.temperature = Number(temperature);
+        this.humidity = Number(humidity);
+        this.pressure = Number(pressure);
+        this.windSpeed = Number(windSpeed);
+    }
+
+    static counter() {
+        if (!Record.currentId)
+            Record.currentId = 0;
+        return Record.currentId++;
+    }
+
+    toString(){
+        return `Reading ID: ${this.id}\n` +
+                `Temperature: ${this.temperature}*C\n` +
+                `Relative Humidity: ${this.humidity}%\n` +
+                `Pressure: ${this.pressure}hpa\n` +
+                `Wind Speed: ${this.windSpeed}m/s\n` +
+                `Weather: ${this.weatherCondition()}`;
+    }
+
+    weatherCondition(){
+        if (this.temperature < 20 && (this.pressure < 700 || this.pressure > 900) && this.windSpeed > 25)
+            return `Stormy`;
+        return 'Not stormy'
+    }
+}
+
+let record1 = new Record(32, 66, 760, 12);
+console.log(record1.toString());
+let record2 = new Record(10, 40, 680, 30);
+console.log(record2.toString());
