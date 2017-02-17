22172c078c0675f9e8a03a1c217a4d23bfd08a3c
Petar Dimitrov <lbmikesmith@gmail.com>
2017-02-17 15:40:26 +0200

    Add files via upload

--- JS Advanced/SampleExam-31Oct2016/04-TitleBar.js
+++ JS Advanced/SampleExam-31Oct2016/04-TitleBar.js
@@ -1,3 +1,49 @@
-/**
- * Created by Petar on 17.2.2017 г..
- */
+class TitleBar{
+    constructor(title){
+        this.title = title;
+        this.links = [];
+        $('.button').on('click', this.toggleDisplay)
+    }
+
+    toggleDisplay(){
+        if ($('.drawer').css('display') == 'none' || $('.drawer').css('display') == undefined )
+            $('.drawer').css('display', 'block');
+        else
+            $('.drawer').css('display', 'none');
+
+    }
+
+    addLink(href, name) {
+        this.links.push($('<a>')
+                        .addClass('menu-link')
+                        .attr('href', href)
+                        .text(name))
+    }
+
+    appendTo(selector){
+        let navMenu = $('<nav>').addClass('menu');
+        for (let link of this.links)
+            navMenu.append(link);
+        let headerHTML = $('<header>').addClass('header')
+            .append($('<div>')
+                .addClass('header-row')
+                .append($('<a>')
+                    .addClass('button')
+                    .html('&#9776;')
+                    .click(() => $('div.drawer').toggle()))
+                .append($('<span>')
+                    .addClass('title')
+                    .text(this.title)))
+            .append($('<div>')
+                .addClass('drawer')
+                .append(navMenu));
+        $(selector).append(headerHTML)
+    }
+}
+
+let header = new TitleBar('Title Bar Problem');
+header.addLink('/', 'Home');
+header.addLink('about', 'About');
+header.addLink('results', 'Results');
+header.addLink('faq', 'FAQ');
+header.appendTo('#container');
