3bcb3026c18948fc864f7f5599b11f975b7a5794
Petar Dimitrov <lbmikesmith@gmail.com>
2017-03-07 10:33:02 +0200

    Add files via upload

--- JS Advanced/08-ModulesBabelTranspiling/Lab/01-PersonModule/app.js
+++ JS Advanced/08-ModulesBabelTranspiling/Lab/01-PersonModule/app.js
@@ -2,3 +2,5 @@ let Person = require('./person');
 result.Person = Person;
 
 // or result.Person = person.Person IF we module.exports = { Person } with {}
+
+// or single line result.Person = require.('./person');
