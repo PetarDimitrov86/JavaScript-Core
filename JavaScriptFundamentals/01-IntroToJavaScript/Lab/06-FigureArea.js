c8ffb46ca11b1fde5c2fc4892e0e1caff452e43d
Petar Dimitrov <lbmikesmith@gmail.com>
2017-05-22 20:00:12 +0300

    Update 06-FigureArea.js

--- JavaScriptFundamentals/01-IntroToJavaScript/Lab/06-FigureArea.js
+++ JavaScriptFundamentals/01-IntroToJavaScript/Lab/06-FigureArea.js
@@ -1,4 +1,4 @@
-function calcArea ([w, h, W, H]){
+function calcArea (w, h, W, H){
     let [bigFigureArea, smallFigureArea] = [W * H, w * h];
     let commonArea = Math.min(w, W) * Math.min(H,h);
     return bigFigureArea + smallFigureArea - commonArea;
