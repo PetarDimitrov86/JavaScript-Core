4ff29508922209c74f06ce06aa8b7d71ac0080b6
Petar Dimitrov <lbmikesmith@gmail.com>
2017-05-22 19:59:23 +0300

    Update 05-StringOfNumbers1ToN.js

--- JavaScriptFundamentals/01-IntroToJavaScript/Lab/05-StringOfNumbers1ToN.js
+++ JavaScriptFundamentals/01-IntroToJavaScript/Lab/05-StringOfNumbers1ToN.js
@@ -1,5 +1,5 @@
 function stringifyNumbs(input){
-    let upperBorder = Number(input[0])
+    let upperBorder = Number(input)
     let result = ""
     for (let i = 1; i<= upperBorder; i++){
         result += i;
