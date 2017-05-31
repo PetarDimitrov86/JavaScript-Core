dded13c9fa2905d697038637aae1a151326c24ea
Petar Dimitrov <lbmikesmith@gmail.com>
2017-05-31 08:49:59 +0300

    Update 09-DiagonalSums.js

--- JavaScriptFundamentals/04-ArraysAndMatrices/Lab/09-DiagonalSums.js
+++ JavaScriptFundamentals/04-ArraysAndMatrices/Lab/09-DiagonalSums.js
@@ -1,18 +1,10 @@
-function calcSum(input){
-    let mainDiagonalSum = 0;
-    let secondaryDiagonalSum = 0;
-    let matrix = input.map(r => r.split(' ').map(Number));
-
-    for (let row in matrix){
-        for (let col in matrix[row]){
-            if (row == col)
-                mainDiagonalSum += matrix[row][col];
-            if (Number(row) + Number(col) == matrix.length - 1)
-                secondaryDiagonalSum += matrix[row][col];
-        }
-    }
-
-    return mainDiagonalSum + ' ' + secondaryDiagonalSum;
+function diagonalSums(matrix) {
+	let mainSum = 0, secondarySum = 0;
+	for (let row = 0; row < matrix.length; row++) {
+		mainSum += matrix[row][row];
+		secondarySum += matrix[row][matrix.length-row-1];
+	}
+	console.log(mainSum + ' ' + secondarySum);
 }
 
 // console.log(calcSum(['3 5 17', '-1 7 14', '1 -8 89']));
