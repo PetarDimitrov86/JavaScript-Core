0af787a914ab038b2b313c8bc1e9db02b9ebb2fd
Petar Dimitrov <lbmikesmith@gmail.com>
2017-06-16 18:45:39 +0300

    Update 02-RosettaStone.js

--- JavaScriptFundamentals/Exam-16October2016/02-RosettaStone.js
+++ JavaScriptFundamentals/Exam-16October2016/02-RosettaStone.js
@@ -36,5 +36,35 @@ solve(
         '23 14 22 0 22',
         '2 17 13 19 20',
         '0 9 0 22 22' ]
-
 );
+
+function solve(input) {
+    // Get size of code
+    let n = Number(input.shift());
+
+    // Initialize code matrix
+    let code = [];
+    for (let i = 0; i < n; i++) {
+        code.push(input.shift().split(' ').map(Number));
+    }
+
+    // Initialize message matrix
+    let matrix = [];
+    for (let row of input) {
+        matrix.push(row.split(' ').map(Number));
+    }
+
+    // Initialize decoded message
+    let result = '';
+
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col< matrix[0].length; col++) {
+            let current = matrix[row][col];
+            let modifier = code[row % code.length][col % code[0].length];
+            result += String.fromCharCode(((current + modifier) % 27) + 64);
+        }
+    }
+
+    result = result.replace(/@/g, ' ');
+    console.log(result);
+}
