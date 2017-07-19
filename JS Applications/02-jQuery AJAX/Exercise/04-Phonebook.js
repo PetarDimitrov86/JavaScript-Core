3e2ab69026b2cd04473ba4e71e8dabcdfd779224
Petar Dimitrov <lbmikesmith@gmail.com>
2017-07-19 14:21:35 +0300

    Add files via upload

---
+++ JS Applications/02-jQuery AJAX/Exercise/04-Phonebook.js
@@ -0,0 +1,55 @@
+function attachEvents() {
+    let hostUrl = 'https://phonebook-nakov.firebaseio.com/phonebook.json';
+    $('#btnLoad').click(loadContacts);
+    $('#btnCreate').click(createNewContact);
+
+    function loadContacts(){
+        $('#phonebook').empty();
+        $.get(hostUrl)
+            .then(fillContactList)
+    }
+
+    function fillContactList(data) {
+        for (let entry in data) {
+            $('#phonebook')
+                .append($('<li>')
+                    .text(`${data[entry].person}: ${data[entry].phone} `)
+                    .append($('<button>')
+                        .text('Delete')
+                        .click(() => {
+                            $.ajax({
+                                method: "DELETE",
+                                url: `https://phonebook-nakov.firebaseio.com/phonebook/${entry}.json`
+                            })
+                                .then(loadContacts)
+                        }))
+                );
+        }
+    }
+
+    function deleteContact(key){
+        let deleteRequest = {
+            method: "DELETE",
+            url: `https://phonebook-nakov.firebaseio.com/phonebook/${key}.json`
+        };
+        $.ajax(deleteRequest)
+            .then(loadContacts)
+    }
+
+    function createNewContact() {
+        let personName = $('#person').val();
+        let phoneNumber = $('#phone').val();
+
+        let obj = {
+            person: personName,
+            phone: phoneNumber
+        };
+
+        $.post(hostUrl, JSON.stringify(obj))
+            .then(() => {
+                $('#person').val('');
+                $('#phone').val('');
+                loadContacts()
+            })
+    }
+}
