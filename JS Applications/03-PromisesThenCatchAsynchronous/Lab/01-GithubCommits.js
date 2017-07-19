2058cdef5bfbb02f83f6491d25e9d344bfc92638
Petar Dimitrov <lbmikesmith@gmail.com>
2017-07-19 18:43:32 +0300

    Add files via upload

---
+++ JS Applications/03-PromisesThenCatchAsynchronous/Lab/01-GithubCommits.js
@@ -0,0 +1,18 @@
+function loadCommits() {
+    let commits = $('#commits');
+    commits.empty();
+    let username = $('#username').val();
+    let repo = $('#repo').val();
+    let baseUrl = `https://api.github.com/repos/${username}/${repo}/commits`;
+    $.get(baseUrl)
+        .then(createList)
+        .catch((error) => commits
+            .append(`<li>Error: ${error.status} (${error.statusText})</li>`));
+
+    function createList(data){
+        for (let commit of data) {
+            commits.append($('<li>')
+                .text(`${commit.commit.author.name}: ${commit.commit.message}`))
+        }
+    }
+}
