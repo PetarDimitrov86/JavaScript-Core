eb88bff7df7a5e077784620d283eb3265b31feef
Petar Dimitrov <lbmikesmith@gmail.com>
2017-07-20 21:55:23 +0300

    Add files via upload

--- JS Advanced/Sample Exam - 7 Nov 2016/04-TableBuilder.js
+++ JS Advanced/Sample Exam - 7 Nov 2016/04-TableBuilder.js
@@ -1,3 +1,34 @@
-/**
- * Created by dimitrovi on 20.7.2017 г..
- */
+function tableBuilder(selector) {
+    function createTable(columnNames) {
+        let table = $('<table>');
+        let tableRow = $('<tr>');
+        for (let column of columnNames){
+            tableRow.append($('<th>')
+                .text(column))
+        }
+        tableRow.append($('<th>Action</th>'));
+        table.append(tableRow);
+        $(selector).html(table)
+    }
+
+    function fillData(dataRows) {
+        for (let row of dataRows){
+            let tableRow = $('<tr>');
+            for (let columnData of row){
+                tableRow.append($('<td>')
+                    .text(columnData))
+            }
+            tableRow.append($('<td>')
+                .append($('<button>')
+                    .text('Delete')
+                    .click(deleteRow)));
+            $(selector + '>table').append(tableRow)
+        }
+    }
+
+    function deleteRow(){
+        $(this).parent().parent().remove();
+    }
+
+    return { createTable, fillData }
+}
