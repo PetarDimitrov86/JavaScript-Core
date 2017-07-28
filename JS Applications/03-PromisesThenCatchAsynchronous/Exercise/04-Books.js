42058d8f5c25cfa53baaba7a720ba82cee2aaa61
Petar Dimitrov <lbmikesmith@gmail.com>
2017-07-28 21:55:52 +0300

    Add files via upload

--- JS Applications/03-PromisesThenCatchAsynchronous/Exercise/04-Books.js
+++ JS Applications/03-PromisesThenCatchAsynchronous/Exercise/04-Books.js
@@ -5,15 +5,17 @@ function attachEvents(){
     const authHeader = { "Authorization": `Basic ${btoa(kinveyUsername + ':' + kinveyPassword)}` };
 
     $('#addBook').click(addNewBook);
+    $('#editBook').click(editBook);
     loadAllBooks();
 
     function loadAllBooks(){
         $('#books').empty();
+        $('#editBookForm').hide();
         $('#books')
             .append($('<tr>')
                 .append($('<td>').text(`ISBN`))
                 .append($('<td>').text('Title'))
-                .append($('<td>').text('Action'))
+                .append($('<td>').text('Author'))
                 .append($('<td>').text('Action')));
         $.get({
             url: baseUrl,
@@ -30,15 +32,15 @@ function attachEvents(){
                     .append($('<td>').text(book.title))
                     .append($('<td>').text(book.author))
                     .append($('<td>')
-                        .append($('<button>').addClass('btn btn-warning').text('Edit').click(editBookInfo))
+                        .append($('<button>').addClass('btn btn-warning').text('Edit').click(() => editBookInfo(book._id)))
                         .append($('<button>').addClass('btn btn-danger').text('Delete').click(() => deleteBook(book._id)))))
         }
     }
 
     function addNewBook() {
-        let isbn = $('#isbn').val();
-        let title = $('#title').val();
-        let author = $('#author').val();
+        let isbn = $('#addBookForm .isbn').val();
+        let title = $('#addBookForm .title').val();
+        let author = $('#addBookForm .author').val();
 
         let newBook = { isbn, title, author };
 
@@ -51,8 +53,39 @@ function attachEvents(){
             .then(loadAllBooks)
     }
 
-    function editBookInfo(){
+    function editBookInfo(bookId){
+        event.preventDefault();
+        $.get({
+            url : baseUrl + '/' + bookId,
+            headers: authHeader
+        })
+            .then(fillEditForm);
 
+        function fillEditForm(data) {
+            $('#editBookForm .isbn').val(data.isbn);
+            $('#editBookForm .title').val(data.title);
+            $('#editBookForm .author').val(data.author);
+            $('#editBookForm #bookId').val(bookId);
+            $('#editBookForm').show();
+        }
+    }
+
+    function editBook() {
+        let bookId = $('#editBookForm #bookId').val();
+        let updatedBook = {
+            isbn: $('#editBookForm .isbn').val(),
+            title: $('#editBookForm .title').val(),
+            author: $('#editBookForm .author').val(),
+        };
+
+        $.ajax({
+            url: baseUrl +'/' + bookId,
+            method: "PUT",
+            contentType: 'application/json',
+            data: JSON.stringify(updatedBook),
+            headers: authHeader
+        })
+            .then(loadAllBooks);
     }
 
     function deleteBook(bookId) {
